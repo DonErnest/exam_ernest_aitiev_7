dae18f7e915aab7658cd429835d2397bcd03f9d0
DonErnest <valeetmihifaveas@gmail.com>
2019-10-19 12:10:51 +0600

    Этап 1: создание модели Poll и Choice, регистрация в панели админа, заполнение  базы данных несколькими опросами и вариантами ответов. файл superuser.txt содержит логини пароль админа

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['question', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['question']
+    fields = ['question', 'created_at']
+    readonly_fields = ['created_at']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['answer_version','poll']
+    list_filter = ['poll']
+    search_fields = ['poll']
+    fields = ['answer_version','poll']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=200, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    answer_version = models.TextField(max_length=300, null=False, blank=False, verbose_name='Вариант ответа')
+    poll = models.ForeignKey('webapp.Poll',on_delete=models.CASCADE, related_name='choices', verbose_name='Опрос')
+
+    def __str__(self):
+        return self.answer_version
